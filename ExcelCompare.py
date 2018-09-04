405e68872927bb779fa5151f2d04da48ba27f050
Jeffrey Wang <jwang@contigo.lan>
2018-09-04 16:31:07 -0700

    Added template code

--- ExcelCompare.py
+++ ExcelCompare.py
@@ -1,4 +1,114 @@
-import tkinter
-top = tkinter.Tk()
-# Code to add widgets will go here...
-top.mainloop()
+import csv, os
+import time
+import re
+from datetime import datetime
+from tkinter import *
+from tkinter import filedialog
+
+def excelCompare(filename1, filename2):
+
+	print("")
+	print("")	
+	print("---------------------FILE PATHS FOR THE TWO CSV FILES:-------------------------")
+	print("Filename 1: "+filename1)
+	print("Filename 2: "+filename2)
+
+	csvFile1 = open(filename1)
+	csvReader1 = csv.reader(csvFile1)
+	csvData1 = list(csvReader1)
+
+	csvFile2 = open(filename2)
+	csvReader2 = csv.reader(csvFile2)
+	csvData2 = list(csvReader2)	
+
+	print("\n===============================================================================")
+	print("----------------------------------BEGIN TEST-----------------------------------")
+
+	counter = 0
+	error_count = []
+
+	pass_counter = 0
+	fail_counter = 0
+	error_list = []
+
+	#CHECK IF FILES HAVE THE SAME AMOUNT OF DATA
+	if len(csvData1) != len(csvData2):
+		print("\nSCRIPT END: The two files have different amount of data - check for missing data!")
+		print("Lines in File 1: %i" % len(csvData1))
+		print("Lines in File 2: %i" % len(csvData2))
+		print("\n===============================================================================")		
+		#Terminate script if different amount of data - check manually
+		exit() 
+
+	#VERIFICATION OF DATA
+	for line in csvData1:
+
+
+		#If the rows match identically
+		if csvData1[counter] == csvData2[counter]: #csvData1[counter] = data of entire row
+			pass_counter +=1
+			#print("Line: "+str(counter+1)+" - PASS")
+
+		#Else identify mismatches and identify any false failures due to formatting/puncutations
+		else:
+			diff = set(csvData1[counter])^set(csvData2[counter])
+			#append differences from the two files into one list called diff_list
+			diff_list = list(diff)
+
+			#clean up all punctuation that are not letters or numbers to prevent false failures
+			clean_list=[]
+			for i in diff_list:
+				a = re.sub('[^A-Za-z0-9]+','', i)
+				b = a.lower()
+				clean_list.append(b)
+				#remove any empty entries
+				if '' in clean_list:
+					clean_list.remove('')
+
+			#VERIFICATION - One Column of mismatch
+			if len(clean_list)==2:
+				if clean_list[0]==clean_list[1]:
+						pass_counter +=1
+				else:
+					print("Line: "+str(counter+1)+" - FAIL")
+					print(diff_list)
+					error_list.append(counter+1)
+					fail_counter += 1				
+			#VERIFICATION - Two Columns of mismatch		
+			elif len(clean_list)==4:
+				if clean_list[0]==clean_list[1] or clean_list[0]==clean_list[2] or clean_list[0]==clean_list[3]:
+						pass_counter +=1
+				elif clean_list[1]==clean_list[0] or clean_list[1]==clean_list[2] or clean_list[1]==clean_list[3]:
+						pass_counter +=1				
+				elif clean_list[2]==clean_list[0] or clean_list[2]==clean_list[1] or clean_list[2]==clean_list[3]:
+						pass_counter +=1
+				elif clean_list[3]==clean_list[0] or clean_list[3]==clean_list[1] or clean_list[3]==clean_list[2]:
+						pass_counter +=1
+				else:
+					print("Line: "+str(counter+1)+" - FAIL")
+					print(diff_list)
+					error_list.append(counter+1)
+					fail_counter += 1
+			#VERIFICATION - Less than One Column of mismatch, OR multiple mismatches (greater than Two)									
+			else:
+				if len(clean_list)<2:
+					pass_counter +=1
+				else:
+					print("Line: "+str(counter+1)+" - FAIL: CHECK ROW FOR INCONSISTENT DATA (Multiple differences in row)")
+					print(diff_list)
+
+					print(clean_list)
+					error_list.append(counter+1)
+					fail_counter += 1
+
+		counter += 1
+
+	print("\n===============================================================================")
+	print("--------------------------------------RESULTS----------------------------------\n")
+	print("Total Lines Tested: " + str(pass_counter+fail_counter))
+	print("PASSED: " + str(pass_counter))
+	print("FAILED: " + str(fail_counter))
+	if fail_counter>0:
+		print("Check lines: ")
+		print(error_list)
+	print("\n===============================================================================")
--- test_Excel_Compare.py
+++ test_Excel_Compare.py
@@ -9,66 +9,65 @@ import tkinter as tk
 from tkinter.scrolledtext import ScrolledText
 from tkinter import ttk, VERTICAL, HORIZONTAL, N, S, E, W
 from tkinter.filedialog import askopenfilename
-
-import summ
+import ExcelCompare
 
 logger = logging.getLogger(__name__)
 
 
 class QueueHandler(logging.Handler):
-    #Class to send logging records to a queue. It can be used from different threads
-    #The ConsoleUi class polls this queue to display records in a ScrolledText widget
-    def __init__(self, log_queue):
-        super().__init__()
-        self.log_queue = log_queue
+	#Class to send logging records to a queue. It can be used from different threads
+	#The ConsoleUi class polls this queue to display records in a ScrolledText widget
+	def __init__(self, log_queue):
+		super().__init__()
+		self.log_queue = log_queue
 
-    def emit(self, record):
-        self.log_queue.put(record)
+	def emit(self, record):
+		self.log_queue.put(record)
 
 
 
 
 class ConsoleUi:
-    """Poll messages from a logging queue and display them in a scrolled text widget"""
+	"""Poll messages from a logging queue and display them in a scrolled text widget"""
 
-    def __init__(self, frame):
-        self.frame = frame
-        # Create a ScrolledText wdiget
-        self.scrolled_text = ScrolledText(frame, state='disabled', height=12)
-        self.scrolled_text.grid(row=0, column=0, sticky=(N, S, W, E))
-        self.scrolled_text.configure(font='TkFixedFont')
-        #self.scrolled_text.tag_config('INFO', foreground='black')
-        #self.scrolled_text.tag_config('DEBUG', foreground='gray')
-        #self.scrolled_text.tag_config('WARNING', foreground='orange')
-        #self.scrolled_text.tag_config('ERROR', foreground='red')
-        #self.scrolled_text.tag_config('CRITICAL', foreground='red', underline=1)
-        # Create a logging handler using a queue
-        self.log_queue = queue.Queue()
-        self.queue_handler = QueueHandler(self.log_queue)
-        formatter = logging.Formatter('%(asctime)s: %(message)s')
-        self.queue_handler.setFormatter(formatter)
-        logger.addHandler(self.queue_handler)
-        # Start polling messages from the queue
-        self.frame.after(100, self.poll_log_queue)
+	def __init__(self, frame):
+		self.frame = frame
+		# Create a ScrolledText wdiget
+		self.scrolled_text = ScrolledText(frame, state='disabled', height=12)
+		self.scrolled_text.grid(row=0, column=0, sticky=(N, S, W, E))
+		self.scrolled_text.configure(font='TkFixedFont')
+		#self.scrolled_text.tag_config('INFO', foreground='black')
+		#self.scrolled_text.tag_config('DEBUG', foreground='gray')
+		#self.scrolled_text.tag_config('WARNING', foreground='orange')
+		#self.scrolled_text.tag_config('ERROR', foreground='red')
+		#self.scrolled_text.tag_config('CRITICAL', foreground='red', underline=1)
+		# Create a logging handler using a queue
+		self.log_queue = queue.Queue()
+		self.queue_handler = QueueHandler(self.log_queue)
+		formatter = logging.Formatter('%(asctime)s: %(message)s')
+		self.queue_handler.setFormatter(formatter)
+		logger.addHandler(self.queue_handler)
+		# Start polling messages from the queue
+		self.frame.after(100, self.poll_log_queue)
 
-    def display(self, record):
-        msg = self.queue_handler.format(record)
-        self.scrolled_text.configure(state='normal')
-        self.scrolled_text.insert(tk.END, msg + '\n', record.levelname)
-        self.scrolled_text.configure(state='disabled')
-        # Autoscroll to the bottom
-        self.scrolled_text.yview(tk.END)
+	def display(self, record):
+		msg = self.queue_handler.format(record)
+		self.scrolled_text.configure(state='normal')
+		self.scrolled_text.insert(tk.END, msg + '\n', record.levelname)
+		self.scrolled_text.configure(state='disabled')
+		# Autoscroll to the bottom
+		self.scrolled_text.yview(tk.END)
 
-    def poll_log_queue(self):
-        # Check every 100ms if there is a new message in the queue to display
-        while True:
-            try:
-                record = self.log_queue.get(block=False)
-            except queue.Empty:
-                break
-            else:
-                self.display(record)
-        self.frame.after(100, self.poll_log_queue)
+	def poll_log_queue(self):
+		# Check every 100ms if there is a new message in the queue to display
+		while True:
+			try:
+				record = self.log_queue.get(block=False)
+			except queue.Empty:
+				break
+			else:
+				self.display(record)
+		self.frame.after(100, self.poll_log_queue)
 
 class FormUi:
 
@@ -78,6 +77,7 @@ class FormUi:
 
 		ttk.Label(self.frame, text='File 1:').grid(column=0, row=0, sticky=W)
 		self.button = ttk.Button(self.frame, text='Browse', width=30, command=self.openFile).grid(column=0, row=1, sticky=(W))
+		logging.info("Button 1: %s" % filename)
 
 		ttk.Label(self.frame, text='File 2:').grid(column=0, row=3, sticky=W)
 		self.button = ttk.Button(self.frame, text='Browse', width=30, command=self.openFile).grid(column=0, row=4, sticky=(W)) 
@@ -93,49 +93,49 @@ class FormUi:
 
 	def submit(self):
 		logger.info("Testing...")
-		print(summ.Summation(5,8))
+		#print(summ.Summation(5,8))
 
 	def close(self):
 		exit()
 
 class App:
 
-    def __init__(self, root):
-        self.root = root
-        root.title('Logging Handler')
-        root.columnconfigure(0, weight=1)
-        root.rowconfigure(0, weight=1)
-        # Create the panes and frames
-        vertical_pane = ttk.PanedWindow(self.root, orient=VERTICAL)
-        vertical_pane.grid(row=0, column=0, sticky="nsew")
-        horizontal_pane = ttk.PanedWindow(vertical_pane, orient=HORIZONTAL)
-        vertical_pane.add(horizontal_pane)
-        
-        form_frame = ttk.Labelframe(horizontal_pane, text="Files")
-        form_frame.columnconfigure(1, weight=1)
-        horizontal_pane.add(form_frame, weight=1)
+	def __init__(self, root):
+		self.root = root
+		root.title('Logging Handler')
+		root.columnconfigure(0, weight=1)
+		root.rowconfigure(0, weight=1)
+		# Create the panes and frames
+		vertical_pane = ttk.PanedWindow(self.root, orient=VERTICAL)
+		vertical_pane.grid(row=0, column=0, sticky="nsew")
+		horizontal_pane = ttk.PanedWindow(vertical_pane, orient=HORIZONTAL)
+		vertical_pane.add(horizontal_pane)
+		
+		form_frame = ttk.Labelframe(horizontal_pane, text="Files")
+		form_frame.columnconfigure(1, weight=1)
+		horizontal_pane.add(form_frame, weight=1)
 
-        console_frame = ttk.Labelframe(horizontal_pane, text="Console")
-        console_frame.columnconfigure(0, weight=1)
-        console_frame.rowconfigure(0, weight=1)
-        horizontal_pane.add(console_frame, weight=1)
+		console_frame = ttk.Labelframe(horizontal_pane, text="Console")
+		console_frame.columnconfigure(0, weight=1)
+		console_frame.rowconfigure(0, weight=1)
+		horizontal_pane.add(console_frame, weight=1)
 
-        #third_frame = ttk.Labelframe(vertical_pane, text="Third Frame")
-        #vertical_pane.add(third_frame, weight=1)
+		#third_frame = ttk.Labelframe(vertical_pane, text="Third Frame")
+		#vertical_pane.add(third_frame, weight=1)
 
-        # Initialize all frames
-        self.form = FormUi(form_frame)
-        self.console = ConsoleUi(console_frame)
-        #self.third = ThirdUi(third_frame)
-        #self.clock = Clock()
-        #self.clock.start()
-        self.root.protocol('WM_DELETE_WINDOW', self.quit)
-        self.root.bind('<Control-q>', self.quit)
-        signal.signal(signal.SIGINT, self.quit)
+		# Initialize all frames
+		self.form = FormUi(form_frame)
+		self.console = ConsoleUi(console_frame)
+		#self.third = ThirdUi(third_frame)
+		#self.clock = Clock()
+		#self.clock.start()
+		self.root.protocol('WM_DELETE_WINDOW', self.quit)
+		self.root.bind('<Control-q>', self.quit)
+		signal.signal(signal.SIGINT, self.quit)
 
-    def quit(self, *args):
-        #self.clock.stop()
-        self.root.destroy()
+	def quit(self, *args):
+		#self.clock.stop()
+		self.root.destroy()
 
 
 
@@ -146,4 +146,4 @@ def main():
 	app.root.mainloop()
 
 if __name__ == '__main__':
-    main()
+	main()
